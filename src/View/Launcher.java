415fe1a421c85a5d31533a77f419f1b57383eb45
hugo4422 <hugoc4422@live.com>
2018-01-26 15:12:49 -0600

    Do the rest

---
+++ src/View/Launcher.java
@@ -0,0 +1,50 @@
+package View;
+
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+	
+	public static BorderPane rootNode;
+	public static SingletonController controller;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		URL fxmlFile = this.getClass().getResource("MainView.fxml");
+		FXMLLoader loader = new FXMLLoader(fxmlFile);
+		rootNode = loader.load();
+		//generate a scenegraph from the fxml file
+		//note that the entire view will be laid out in the fxml file
+		
+		Scene scene = new Scene(rootNode, 600, 400);
+		primaryStage.setTitle("Menu");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		controller = SingletonController.getInstance();
+		controller.setRootNode((BorderPane) rootNode);
+		
+		fxmlFile = this.getClass().getResource("Menu.fxml");
+		loader = new FXMLLoader(fxmlFile);
+		
+		loader.setController(controller);
+		Parent view = loader.load();
+		rootNode.setCenter(view);
+		//build a scene with the scenegraph as its root node
+		
+		
+			    
+		
+	}
+}
---
+++ src/View/SingletonController.java
@@ -0,0 +1,50 @@
+package View;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class SingletonController implements Initializable {
+	
+	private static SingletonController view;
+	private BorderPane rootNode;
+	@FXML private Button bList;
+	@FXML private Button bQuit;
+	@FXML private Button bBack;
+
+	protected SingletonController() {
+		// Exists only to defeat instantiation.
+	}
+
+	public static SingletonController getInstance() {
+		if (view == null) {
+			view = new SingletonController();
+		}
+		return view;
+	}
+	
+	public void setRootNode(BorderPane rootNode) {
+		this.rootNode = rootNode;
+	}
+
+	@FXML void handleMenuEvent(ActionEvent event) throws IOException {
+		if(event.getSource() == bList) {
+			System.out.println("Author List Clicked");
+		}
+		else if(event.getSource() == bQuit) {
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
